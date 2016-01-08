28cbe9b711e00e1cb135b800b78323fc05c84fbc
Roman Zimmermann <roman@more-onion.com>
2016-01-08 10:45:34 +0100

    Copy of campaignion_recent_supporters.

---
+++ src/Polling.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace \Drupal\campaignion_recent_supporters;
+
+class Polling implements \Drupal\polling\FieldTypePollingInterface {
+  public static function instance($entity, $field, $instance) {
+  }
+
+  public function __construct() {
+
+  }
+
+  public function getData() {
+  }
+}
