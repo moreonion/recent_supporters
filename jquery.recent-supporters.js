23e1fb65f1c7d31a449fa1f9ffa6205a9aaf0b1f
Roman Zimmermann <roman@more-onion.com>
2016-01-08 15:08:22 +0100

    Fix behavior when showCountry is true but the country is unknown.

--- jquery.recent-supporters.js
+++ jquery.recent-supporters.js
@@ -190,7 +190,7 @@
       if (settings.showCountry) {
         var countryCode = supporter.country ? supporter.country.toLowerCase() : "no-cc";
         var countryName = supporter.country in settings.countries ? settings.countries[supporter.country] : '';
-        $li.prepend('<span title="'+countryName+'" class="country flag flag-'+countryCode+'">'+supporter.country+'</span> ');
+        $li.prepend('<span title="'+countryName+'" class="country flag flag-'+countryCode+'">'+countryCode+'</span> ');
       }
       if (settings.showComment) {
         $li.append('<span class="comment">' + supporter.comment + '</span> ')
